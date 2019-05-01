9ceb805717d4d418baa3f0ffdb3437a516fb5ed5
ldasmdn <ldasmdn@DESKTOP-FOLEK8M>
2019-05-01 20:57:30 +0900

    메이븐 프로젝트

---
+++ 170317_GradleTest/src/main/java/JodaTimeTest.java
@@ -0,0 +1,10 @@
+import org.joda.time.DateTime;
+
+public class JodaTimeTest {
+	public static void main(String[] args) {
+		
+		DateTime dateTime = new DateTime();
+		System.out.println(dateTime);
+		
+	}
+}
