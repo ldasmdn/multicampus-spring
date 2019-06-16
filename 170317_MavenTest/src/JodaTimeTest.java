9a07bd2e21fdaac63e83f614ad7696a363cd8302
ldasmdn <ldasmdn@DESKTOP-CGFB7HO>
2019-06-16 12:31:41 +0900

    Maven 프로젝트 생성

--- 170317_MavenTest/src/JodaTimeTest.java
+++ 170317_MavenTest/src/JodaTimeTest.java
@@ -1,8 +1,10 @@
 import org.joda.time.DateTime;
 
 public class JodaTimeTest {
+	
 	public static void main(String[] args) {
 		
+		// JodaTime API
 		DateTime dateTime = new DateTime();
 		System.out.println(dateTime);
 		
